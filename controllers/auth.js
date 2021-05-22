be8fdbd234dff4fa89da6c550e611dc83cfb23dc
susilreact <jenasusil@outlook.com>
2021-05-22 23:33:59 +0530

    Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 const { validationResult } = require('express-validator');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
 
 exports.signup = (req, res) => {
     const errors = validationResult(req);
@@ -24,8 +26,75 @@ exports.signup = (req, res) => {
     });
 };
 
+exports.signin = (req, res) => {
+    const errors = validationResult(req);
+    const { email, password } = req.body;
+    
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            error: errors.array()[0].msg,
+        });
+    }
+
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
+           return res.status(400).json({
+                error: 'USER email not exists',
+            });
+        }
+
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: 'email and password do not match',
+            });
+        }
+
+        //create token
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+
+        // put token in browser cookie
+        res.cookie('token', token, { expire: new Date() + 9999 });
+
+        //send response to frontend
+        const { _id, name, email, role } = user;
+        res.json({ token, user: { _id, name, email, role } });
+    });
+
+    
+};
+
 exports.signout = (req, res) => {
+    res.clearCookie('token');
     res.json({
-        message: 'User Signout',
+        message: 'User Signout Successfully',
     });
 };
+
+//proteacted routes
+
+exports.isSignedIn = expressJwt({
+    secret: process.env.SECRET,
+    algorithms:['RS256'],
+     //userProperty: "auth",
+});
+
+exports.isAuthenticated =(req, res, next)=>{
+    const checker = req.profile &&req.auth && req.profile._id == req.auth._id
+
+    if(!checker){
+        return res.status(403).json({
+            error:"ACCESS DENIED"
+        })
+    }
+    next();
+
+}
+
+exports.isAdmin =(req, res, next)=>{
+    if(res.profile.role === 0){
+        return res.status(403).json({
+            error:"YOU ARE NOT ADMIN, access denied"
+        })
+    }
+    next();
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { signout, signup } = require('../controllers/auth');
+const { signout, signup, signin, isSignedIn } = require('../controllers/auth');
 const { check } = require('express-validator');
-
 
 router.post(
     '/signup',
@@ -17,8 +16,21 @@ router.post(
     ],
     signup
 );
+
+router.post(
+    '/signin',
+    [
+        check('email', 'Email is required').isEmail(),
+        check('password', 'password is required').isLength({
+            min: 6,
+        }),
+    ],
+    signin
+);
 router.get('/signout', signout);
 
+router.get('/testroute', isSignedIn, (req, res) => {
+    res.send('A protected Route');
+});
 
-
-module.exports =router;
+module.exports = router;
